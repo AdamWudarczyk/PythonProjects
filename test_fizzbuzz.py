50c4775baace6777093b3c51e416612f7d173e85
AdamWudarczyk <70765358+AdamWudarczyk@users.noreply.github.com>
2021-02-19 11:03:11 +0100

    Create test_fizzbuzz.py

---
+++ test_fizzbuzz.py
@@ -0,0 +1,17 @@
+import unittest
+import fizzbuzz
+
+class MyTestCase(unittest.TestCase):
+
+    def test_fizz(self):
+        self.assertEqual(fizzbuzz,"fiz")
+
+    def test_buzz(self):
+        self.assertEqual()
+
+    def test_fizzbuzz(self):
+        self.assertEqual()
+
+
+if __name__ == '__main__':
+    unittest.main()
